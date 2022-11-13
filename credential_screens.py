277c639452caafb011ace103b05aedba2524ffb4
Rishi-k-s <rishikrishna.sr@gmail.com>
2022-11-14 01:04:42 +0530

    my sql connection initiated

--- credential_screens.py
+++ credential_screens.py
@@ -1,5 +1,29 @@
+import mysql.connector
+mysql.connector.connection(
+    host = "sql.freedb.tech",
+    username = "freedb_main_user2",
+    password = "GcFhae@3x9j4h5V",
+    database = "freedb_housekeepingmngmnt"
+)
 def login():
+    # have to enter the username and password and the hotel uid when any type of user logs in
+    #aftr logged in hav to return sm value
     pass
 
 def signup():
+    #if the signup is 
     pass
+
+def make_hs_kep_user():
+    # the code for making 
+    pass
+"""
+Signup is only for super admin and user
+super admins add housekeeers for the place
+"""
+"""If the signup is as guest then ask for username and password, each user name should be unique
+   if the sign up is as a Housekeeper ask for the same
+   but if its as a super admin ask for address, contact info and unique place id
+   ?? do we neeed housekeeper user details
+
+"""
--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import credential_screens
+import mysql
+# all dashboard screen would be here
 
+# the guest dashboard
+def guest_dashboard():
+    pass
+
+#the housekeeper dashboard
+def hs_kep_dashboard():
+    pass
+
+#super admin dashboard
+def s_admin_dashboard():
+    pass
